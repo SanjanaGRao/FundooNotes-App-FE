3dad32780f0156718770735db284f62c2ad3c4f2
Sanjana Rao <sanjanagrao99@gmail.com>
2021-10-27 20:45:46 +0530

    Refactored: UI of the Create Account Page

--- src/components/Pages/CreateAccount.jsx
+++ src/components/Pages/CreateAccount.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import '../css/createAccountPage.css'
-import { Grid, Paper, Avatar, Typography, TextField, Button } from '@material-ui/core';
+import accounts from "../images/account.svg"
+import { Grid, Typography, TextField, Button } from '@material-ui/core';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import CollectionsBookmarkSharpIcon from '@mui/icons-material/CollectionsBookmarkSharp';
 import Checkbox from '@material-ui/core/Checkbox';
-
 
 export class CreateAccount extends React.Component {
     constructor(props) {
@@ -15,75 +14,183 @@ export class CreateAccount extends React.Component {
             email: "",
             password: "",
             passwordConfirmation: "",
+            showPassword: false,
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+    }
+    isValidName(name) {
+        const onlyAlphabetsAndSpace = /^[a-zA-Z\s]+$/;
+        return onlyAlphabetsAndSpace.test(name);
     }
     handleSubmit(event) {
         console.log("Form Submitted");
         event.preventDefault();
     }
     handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
+        var name = event.target.value;
+        if (name) {
+            var isValid = this.isValidName(name);
+        }
+        if (isValid) {
+            this.setState({
+                [event.target.name]: event.target.value,
+            });
+        } else {
+            this.setState({
+                [event.target.name]: event.target.value,
+            });
+        }
     }
     render() {
-        const paperStyle = { padding: '30px 20px', width: 350, margin: "20px auto", backgroundColor: '#EAFFD0'};
         const headerStyle = { margin: 0 };
-        const avatarStyle = { backgroundColor: '#a696c8' }
         return (
-            <Grid>
-                <Paper elevation={12} style={paperStyle}>
-                    <Grid align='center'>
-                        <Avatar style={avatarStyle}>
-                            {/* alt="fundoo Notes"
-                            src="/fundoonotes-app/src/components/images/pngtree-notes-icon-vector-png-image_2340328.jpg"
-                            sx={{ width: 56, height: 56 }}  */}
-                            <CollectionsBookmarkSharpIcon />
-                        </Avatar>
-                        <h2 class="rainbow" style={headerStyle}>
-                            <span>f</span>
-                            <span>u</span>
-                            <span>n</span>
-                            <span>d</span>
-                            <span>o</span>
-                            <span>o</span>
-                            <span>N</span>
-                            <span>o</span>
-                            <span>t</span>
-                            <span>e</span>
-                            <span>s</span>
-                        </h2>
-                        <br />
-                        <Typography variant='h7' color='primary' gutterBottom><b><i>Create your fundooNotes Account</i></b></Typography>
-                    </Grid>
-                    <form>
-                        <br />
-                        <TextField required  id="outlined-basic" color= 'primary' type='text' label='First Name' placeholder="Enter your first name"  onChange={this.handleChange}/>
-                        <br />
-                        <TextField required id="outlined-basic" color= 'primary' type= 'text' label='Last Name' placeholder="Enter your last name"  onChange={this.handleChange}/>
-                        <br />
-                        <TextField required id="outlined-basic" color= 'primary' type= 'email' label='Email' placeholder="Enter your email"  onChange={this.handleChange}/>
-                        <br />
-                        <TextField required id="outlined-basic" color= 'primary' type= 'password' label='Password' placeholder="Enter your password"  onChange={this.handleChange} />
-                        <br />
-                        <TextField required id="outlined-basic" color= 'primary' type= 'password' label='Confirm Password' placeholder="Confirm your password"  onChange={this.handleChange}/>
-                        <br />
-                        <br />
-                        <FormControlLabel
-                            control={<Checkbox name="checkedA" style={{transform: "scale(0.75)"}} />}
-                            label= {<span style={{ fontSize: '0.75rem' }}>I accept the terms and conditions.</span>}
-                            onChange={this.handleChange}
-                        />
-                        <br /><br />
-                        <Button type='submit' variant='contained' color='primary'><b>Sign up</b></Button>
-                    </form>
-                </Paper>
-            </Grid>
+            <div className="imgBox">
+                <div className="outerBox">
+                    <div className="outerPadding"></div>
+                    <div>
+                        <div>
+                            <Grid align='left' className="headingAndSubHeading">
+                                <h1 class="rainbow" style={headerStyle}>
+                                    <span>F</span>
+                                    <span>u</span>
+                                    <span>n</span>
+                                    <span>d</span>
+                                    <span>o</span>
+                                    <span>o</span>
+                                    <span>N</span>
+                                    <span>o</span>
+                                    <span>t</span>
+                                    <span>e</span>
+                                    <span>s</span>
+                                </h1>
+                                <br />
+                                <Typography variant='h6' gutterBottom><b>Create your FundooNotes Account</b></Typography>
+                            </Grid>
+                            <form onSubmit={this.handleSubmit} id="form" name="form">
+                                <div className="innerImg">
+                                    <div className="inputBox">
+                                        <br />
+                                        <div className="names" align="left">
+                                            <div className="firstName">
+                                                <TextField
+                                                    required
+                                                    variant="outlined"
+                                                    name="firstName"
+                                                    color='primary'
+                                                    type='text'
+                                                    label='First Name'
+                                                    placeholder="Enter your first name"
+                                                    size="small"
+                                                    value={this.state.firstName}
+                                                    onChange={this.handleChange}
+                                                />
+                                            </div>
+                                            <div className="lastName">
+                                                <TextField
+                                                    required
+                                                    variant="outlined"
+                                                    name="lastName"
+                                                    color='primary'
+                                                    type='text'
+                                                    label='Last Name'
+                                                    placeholder="Enter your last name"
+                                                    size="small"
+                                                    value={this.state.lastName}
+                                                    onChange={this.handleChange}
+                                                />
+                                            </div>
+                                        </div>
+                                        <br />
+                                        <div className="email" align="left">
+                                            <TextField
+                                                required
+                                                fullWidth
+                                                id="full-width-text-field"
+                                                variant="outlined"
+                                                name="email"
+                                                color='primary'
+                                                type='email'
+                                                label='Email'
+                                                placeholder="xyz@example.com"
+                                                autoComplete="email"
+                                                size="small"
+                                                helperText="Your mail can consist of letters, numbers and periods"
+                                                value={this.state.email}
+                                                onChange={this.handleChange}
+                                            />
+                                        </div>
+                                        <br />
+                                        <div className="passwords" align="left">
+                                            <div className="pass">
+                                                <TextField
+                                                    required
+                                                    fullwidth
+                                                    variant="outlined"
+                                                    name="password"
+                                                    color='primary'
+                                                    type="password"
+                                                    label='Password'
+                                                    size="small"
+                                                    placeholder="Enter your password"
+                                                    value={this.state.password}
+                                                    onChange={this.handleChange}
+                                                />
+                                            </div>
+                                            <div className="confirmPassword">
+                                                <TextField
+                                                    required
+                                                    fullwidth
+                                                    variant="outlined"
+                                                    name="passwordConfirmation"
+                                                    color='primary'
+                                                    type='password'
+                                                    label='Confirm Password'
+                                                    placeholder="Confirm your password"
+                                                    size="small"
+                                                    value={this.state.passwordConfirmation}
+                                                    onChange={this.handleChange}
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="message">
+                                            <span>
+                                                Use 8 or more characters with a mix of letters, numbers & symbols
+                                            </span>
+                                        </div>
+                                        <br />
+                                        <div className="showPassword">
+                                            <FormControlLabel
+                                                control={
+                                                    <Checkbox
+                                                        value="allowExtraEmails"
+                                                        color="primary"
+                                                    />
+                                                }
+                                                label={<span style={{ fontSize: '0.75rem' }}>Show Password</span>}
+                                                onChange={this.handleChange}
+                                            />
+                                        </div>
+                                        <div className="signInSignUp">
+                                            <div className="signIn">
+                                                <Button variant="text" style={{ textTransform: 'none' }} color='primary' ><b>Sign in instead</b></Button>
+                                            </div>
+                                            <div className="create">
+                                                <Button type='submit' variant='contained' color='primary' onChange={this.handleSubmit}><b>Create</b></Button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="img">
+                                        <img src={accounts} width={260} height={244} style={{ verticalAlign: 'middle' }} />
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
+
     }
 }
-
-
 
